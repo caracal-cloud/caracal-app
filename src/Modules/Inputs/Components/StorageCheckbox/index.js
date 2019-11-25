47c4af890610f31b7d9255f270e5f004f05ac70e
Pedro Nauck <pedronauck@gmail.com>
2019-11-25 18:06:28 -0300

    feat: only allow arcgis if connected

--- src/Modules/Inputs/Components/StorageCheckbox/index.js
+++ src/Modules/Inputs/Components/StorageCheckbox/index.js
@@ -1,15 +1,27 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
+import { useMachine } from '@xstate/react'
 import { CheckboxGroup, Checkbox, formUtils } from 'Modules/Core'
+import { storageCheckboxMachine } from './machines/storeCheckbox'
 
 export function StorageCheckbox({ className, ...form }) {
+  const [state] = useMachine(storageCheckboxMachine)
+  const isLoading = state.matches('loading')
+  const isConnected = state.context.connected
+
   return (
     <div sx={{ mt: 3 }} className={className}>
       <CheckboxGroup vertical label="Outputs">
-        <Checkbox {...formUtils.getCheckboxProps('outputKml', form)}>
+        <Checkbox
+          {...formUtils.getCheckboxProps('outputKml', form)}
+          disabled={isLoading}
+        >
           KML (Google Earth)
         </Checkbox>
-        <Checkbox {...formUtils.getCheckboxProps('outputAgol', form)}>
+        <Checkbox
+          {...formUtils.getCheckboxProps('outputAgol', form)}
+          disabled={isLoading || !isConnected}
+        >
           ArcGIS Online
         </Checkbox>
       </CheckboxGroup>
---
+++ src/Modules/Inputs/Components/StorageCheckbox/machines/storeCheckbox.js
@@ -0,0 +1,45 @@
+import * as R from 'ramda'
+import { Machine, assign } from 'xstate'
+import { getArcgisAccount } from 'Modules/Outputs/Modules/ArcgisOnline/Api'
+
+const services = {
+  getAccount: async () => {
+    const res = await getArcgisAccount()
+    return res.data
+  }
+}
+
+const actions = {
+  setConnection: assign((ctx, ev) => {
+    return R.assoc('connect', ev.data.isConnected, ctx)
+  })
+}
+
+const machine = Machine({
+  id: 'storageCheckbox',
+  initial: 'loading',
+  context: {
+    connected: false
+  },
+  states: {
+    loading: {
+      invoke: {
+        src: 'getAccount',
+        onDone: 'success',
+        onError: 'failure'
+      }
+    },
+    failure: {
+      type: 'final'
+    },
+    success: {
+      entry: 'setConnection',
+      type: 'final'
+    }
+  }
+})
+
+export const storageCheckboxMachine = machine.withConfig({
+  actions,
+  services
+})
