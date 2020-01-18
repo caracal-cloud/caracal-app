2a380e61255a27fa497f07ee4e8df3f95957f0b8
Pedro Nauck <pedronauck@gmail.com>
2020-01-18 16:29:53 -0300

    fix: add some error handler

--- src/Modules/Core/Utils/handlers.js
+++ src/Modules/Core/Utils/handlers.js
@@ -7,10 +7,12 @@ const dataResponse = R.uncurryN(2, type => R.path(['response', 'data', type]))
 const formSubmitHandler = (_, ev) => {
   const msg = dataResponse('message', ev.data)
   const detail = dataResponse('detail', ev.data)
+  const error = dataResponse('error', ev.data)
   const nonFieldErrors = dataResponse('nonFieldErrors', ev.data)
 
   if (msg) return message.error(humanize(msg))
   if (detail) return message.error(humanize(detail))
+  if (error) return message.error(humanize(error))
   if (nonFieldErrors && nonFieldErrors.length > 0) {
     return message.error(humanize(nonFieldErrors[0]))
   }
